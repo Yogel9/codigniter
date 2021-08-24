4c87a5a8ec7cb799b1e428acc7b904a150cb0c0d
Антон Довиденков <79474789+Yogel9@users.noreply.github.com>
2021-08-24 15:24:26 +0400

    Урок 12
    Переноска уже готовой верстки

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,142 @@
-	
+	<!--
 </body>
+</html>-->
+<!-- Footer start -->  
+        </div>
+
+        <!-- MENU start -->
+        <div class="col-lg-3 col-lg-pull-9"> <!-- Меняем блоки местами col-lg-pull-9 -->
+          
+          <div class="panel panel-info hidden-xs">
+            <div class="panel-heading"><div class="sidebar-header">Поиск</div></div>
+            <div class="panel-body">
+              <form role="search">
+                <div class="form-group">
+                  <div class="input-group">
+                    <input type="search" class="form-control input-lg" placeholder="Ваш запрос">
+                    <div class="input-group-btn">
+                      <button class="btn btn-default btn-lg" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                    </div>
+                  </div>
+                </div>
+              </form> 
+            </div>
+          </div>
+    
+          <div class="panel panel-info">
+            <div class="panel-heading"><div class="sidebar-header">Вход</div></div>
+            <div class="panel-body">
+
+              <form role="form">
+                <div class="form-group">
+                  <input type="text"  class="form-control input-lg" placeholder="Логин">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control input-lg" placeholder="Пароль">
+                </div>
+
+                <button type="submit" class="btn btn-warning pull-right">вход</button>
+
+              </form>
+
+            </div>
+          </div>  
+
+
+          <div class="panel panel-info">
+            <div class="panel-heading"><div class="sidebar-header">Новости</div></div>
+            <div class="panel-body">
+              
+              <p>31.02.2017</p>
+              <p>Мы запустили новый сервис</p>
+              
+            </div>
+          </div>
+
+
+          <div class="panel panel-info">
+            <div class="panel-heading"><div class="sidebar-header">Рейтинг фильмов</div></div>
+            <div class="panel-body">
+                
+                <ul class="list-group">
+                  
+                  <li class="list-group-item list-group-warning">
+                    <span class="badge">8.9</span>
+                    <a href="show.html">Интерстеллар</a>
+                  </li>
+
+                  <li class="list-group-item list-group-warning">
+                    <span class="badge">8.7</span>
+                    <a href="#">Безумный макс</a>
+                  </li>                  
+
+                  <li class="list-group-item list-group-warning">
+                    <span class="badge">8.3</span>
+                    <a href="#">Матрица</a>
+                  </li>                  
+
+                  <li class="list-group-item list-group-warning">
+                    <span class="badge">8.1</span>
+                    <a href="#">Облачный атлас</a>
+                  </li>
+
+                  <li class="list-group-item list-group-warning">
+                    <span class="badge">8.9</span>
+                    <a href="show.html">Интерстеллар</a>
+                  </li>
+
+                  <li class="list-group-item list-group-warning">
+                    <span class="badge">8.7</span>
+                    <a href="#">Безумный макс</a>
+                  </li>                  
+
+                  <li class="list-group-item list-group-warning">
+                    <span class="badge">8.3</span>
+                    <a href="#">Матрица</a>
+                  </li>                  
+
+                  <li class="list-group-item list-group-warning">
+                    <span class="badge">8.1</span>
+                    <a href="#">Облачный атлас</a>
+                  </li>
+
+                </ul>
+
+            </div>
+          </div>  
+          
+
+
+        </div>      
+
+      <!-- MENU end -->
+
+
+        </div>
+
+      </div>
+
+      <div class="clear"></div>
+
+    </div>
+
+
+    <footer>
+      
+      <div class="container">
+        <p class="text-center"> <a href="http://ru.wh-db.com">WH-DB.COM</a> </p>
+      </div>
+
+    </footer>
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/assets/js/bootstrap.min.js"></script>
+  </body>
 </html>
+
+<!-- Footer end -->
